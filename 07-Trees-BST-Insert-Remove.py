fadd8b02097916d250d86e76d783ca2e7962b4ea
CTC3PO <chautrancmt26@gmail.com>
2022-12-10 22:26:18 -0500

    Add tree-related codes

--- 07-Trees-BST-Insert-Remove.py
+++ 07-Trees-BST-Insert-Remove.py
@@ -1,28 +1,41 @@
-# Codes for REMOVING a node in BST
+#INSERTION: 
+def insert (root,val):
+    if not root:
+        return TreeNode(val)
+    if val > root.val: 
+        root.right = insert(root.right, val) 
+    elif val < root.val: 
+        root.left = insert(root.left, val)
+    return root 
 
-def minValueNode(root):
-    curr = root 
-    while curr and curr.left:
-        curr = curr.left
-    return curr
-
+# DELETION
 def remove(root, val):
     if not root: 
-        return None
-    
+        return None     
     if val > root.val: 
         root.right = remove(root.right, val)
     elif val < root.val:
         root.left = remove(root.left, val)
     else: 
+        #simple case: node has 0 or 1 child: 
         if not root.left: 
             return root.right
         elif not root.right: 
             return root.left
+        #else: recursively look and remove the node 
         else: 
             minNode = minValueNode(root.right)
             root.val = minNode.val
             root.right = remove(root.right, minNode.val) 
     return root 
 
+# Helper function for Remove function:
+def minValueNode(root):
+    curr = root 
+    while curr and curr.left:
+        curr = curr.left
+    return curr
+
+# Note: when removing a root node, better to replace it with a leaf note
+# using either the largest left-leaf node, or the smallest right-leaf node
     
---
+++ 07-Trees-BST.py
@@ -0,0 +1,26 @@
+# BST is sorted tree, left sub-tree < root, right sub-tree > root
+# to search: go to the right of the tree if target > root value
+# go to the right of the tree if target < root value
+# recursively look for target and traverse the tree until finding
+# the target 
+
+def search(root, target):
+    if not root: 
+        return False
+    if target > root.val: 
+        return search(root.right, target)
+    elif target < root.val: 
+        return search(root.left, target)
+    else: 
+        return  True
+
+
+# Balanced tree (if the height of the left and right is roughly equal)
+# mearning the difference in their level should be close to 1
+
+# COMPLEXITY: O (h) - the heihgt of the tree
+# but for simplicity: we use O(logn)
+
+# WHY BST OVER SORTED ARRAY (using binary search?) 
+# --> Insert and Delete is more efficient than array 
+# Array: O(n) vs. BST (insert/delete): O(logn)
