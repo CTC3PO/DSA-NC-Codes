acda270f2e9b02a39545e93fda732a3c28b63036
CTC3PO <chautrancmt26@gmail.com>
2022-12-24 12:10:39 -0500

    edit codes

--- 06-Search-SearchRange.py
+++ 06-Search-SearchRange.py
@@ -1,3 +1,22 @@
+#Implementation of binary search
+# TIME COMPLEXITY: O(logn) - because we eliminate/narrow down
+# to half the values each time (2^x = n, so x = logn)
+
+def binarySearch(arr, target):
+    L,R = 0, len(arr) - 1
+
+    while L <= R:
+        mid = (L + R) // 2
+
+        if target > arr[mid]:
+            L = mid + 1
+        elif target < arr[mid]:
+            R = mid - 1
+        else: 
+            return mid
+    # Return -1 if found no target (target not in array)
+    return -1   
+
 def binarySearchWithRange(low, high):
     while low < high:
         mid = (low + high) /2
@@ -5,7 +24,7 @@ def binarySearchWithRange(low, high):
         #check isCorrect condition for mid
         # then move high or low accordingly
         if isCorrect(mid) > 0:
-            high = mid -1
+            high = mid - 1
         elif isCorrect(mid) < 0:
             low = mid + 1
         else: 
--- 07-Trees-BST-Insert-Remove.py
+++ 07-Trees-BST-Insert-Remove.py
@@ -5,10 +5,16 @@ class TreeNode:
         self.left = None
         self.right = None
 
+#BST: base case: has 0/1 child
 #INSERT a new node and return the root of the BST
 def insert (root,val):
+    # if tree is empty (not root), add the new TreeNode to the tree
     if not root:
         return TreeNode(val)
+    # otherwise, compare the val to root.val, pass it to the left
+    # of the tree if val < root.val, pass to the right of the 
+    # tree if val > root.val, then recursively do that until 
+    # inserting in the right position 
     if val > root.val: 
         root.right = insert(root.right, val) 
     elif val < root.val: 
@@ -16,33 +22,44 @@ def insert (root,val):
     return root 
 
 # REMOVE a node and return the root of the BST
+# When remove, 2 cases: 1.node has 0 or 1 child
+# 2. node has 2 children. 
 def remove(root, val):
+    # if the tree is empty (no root node), return nothing
     if not root: 
         return None     
+
+    # check if the val is > or < than root.val
     if val > root.val: 
         root.right = remove(root.right, val)
     elif val < root.val:
         root.left = remove(root.left, val)
+
+    #otherwise, check if the node has only 1 node (either
+    # left or right, if yes, remove its child)
     else: 
         #simple case: node has 0 or 1 child: 
         if not root.left: 
             return root.right
         elif not root.right: 
             return root.left
-        #else: recursively look and remove the node 
+        
+        # else: the node has 2 children, 
+        # recursively look and remove the node, replace
+        # the node with the min node of the right sub-tree 
         else: 
             minNode = minValueNode(root.right)
             root.val = minNode.val
             root.right = remove(root.right, minNode.val) 
     return root 
 
-# Helper function - Return the min value node of the BST
+# Helper function - to get the min value node of the right sub-tree 
 def minValueNode(root):
     curr = root 
     while curr and curr.left:
         curr = curr.left
     return curr
 
-# Note: when removing a root node, better to replace it with a leaf note
+# Note: when removing a root node, better to replace it with a leaf node
 # using either the largest left-leaf node, or the smallest right-leaf node
     
